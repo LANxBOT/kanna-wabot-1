dd16f41edc105cbad0bc413b21ff4400d0355dfa
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-04 14:58:44 +0800

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -17,7 +17,7 @@ let username = conn.getName(who)
     await this.send3ButtonLoc(m.chat, logo, `
 *hai, ${ucapan()}*
 
-${user.banned ? 'kamu dibanned' : 'Saya adalah KANNABOT, salah satu bot Whatsapp. harap tidak spam/telpon/minta save kemonor ini. Ada yang bisa saya bantu? (ㆁωㆁ)'}
+${user.banned ? 'kamu dibanned' : 'Saya adalah LynXzy, salah satu bot Whatsapp. harap tidak spam/telpon/send virtex/minta save kemonor ini. Ada yang bisa saya bantu?'}
 `.trim(), wm, user.register ? '⋮☰ Menu' : 'Verify', user.register ? '.menu' : `.daftar ${username}.13`, 'Rules', '.rules', 'Owner', '.owner', m)
     user.pc = new Date * 1
 }
